3580847dfb13ea4a5b037676e8bd8336f0dca476
SeongkookKIM <ksk8646@naver.com>
2023-09-25 14:33:47 +0900

    슬라이드 페이지 완료

--- rich-club/src/Pages/Bless.js
+++ rich-club/src/Pages/Bless.js
@@ -1,10 +1,66 @@
 import React from "react";
 import { Section } from "react-fullpage";
+import Slide from "../SlideComponents/Slide";
 
 function Bless() {
+  const data = [
+    {
+      src: "/assets/image/NFT/G-bless/Pharaoh.png",
+      title: "Pharaoh",
+      description: "Enjoy the sparkling blessings of the generous Buddha.",
+    },
+    {
+      src: "/assets/image/NFT/G-bless/genie.png",
+      title: "genie",
+      description: "It will make your wish come true.",
+    },
+    {
+      src: "/assets/image/NFT/G-bless/Ganesha.png",
+      title: "Ganesha",
+      description: "It will bring you dazzling good luck.",
+    },
+    {
+      src: "/assets/image/NFT/G-bless/Elon_Musk.png",
+      title: "Elon_Musk",
+      description: "Dream of a shining future with him.",
+    },
+    {
+      src: "/assets/image/NFT/G-bless/Great_Sage.png",
+      title: "Great_Sage",
+      description:
+        "The Great Sage Heaven's Equal. He will satisfy you with his brilliant magical tricks.",
+    },
+    {
+      src: "/assets/image/NFT/G-bless/oil_money.png",
+      title: "oil_money",
+      description: "Enjoy a luxurious life with him.",
+    },
+    {
+      src: "/assets/image/NFT/G-bless/Anubis.png",
+      title: "Anubis",
+      description: "He will share the offerings of the dead with you.",
+    },
+    {
+      src: "/assets/image/NFT/G-bless/jesus.png",
+      title: "jesus",
+      description: "He will save you with his grace.",
+    },
+    {
+      src: "/assets/image/NFT/G-bless/Tony_Stark.png",
+      title: "Tony_Stark",
+      description: "Express yourself with confidence like him.",
+    },
+    {
+      src: "/assets/image/NFT/G-bless/Pharaoh.png",
+      title: "Pharaoh",
+      description: "Enjoy the taste of power and gold with him.",
+    },
+  ];
   return (
     <Section className="bless">
-      <div className="bless-inner">Bless</div>
+      <div className="bless-inner">
+        <Slide data={data} />
+      </div>
     </Section>
   );
 }
--- rich-club/src/Pages/Born.js
+++ rich-club/src/Pages/Born.js
@@ -1,10 +1,57 @@
 import React from "react";
 import { Section } from "react-fullpage";
+import Slide from "../SlideComponents/Slide";
 
 function Born() {
+  const data = [
+    {
+      src: "/assets/image/NFT/Born/triceratops.png",
+      title: "triceratops",
+      description:
+        "A herbivorous dinosaur characterized by three horns on its head and a wide frill.",
+    },
+    {
+      src: "/assets/image/NFT/Born/Ankylosaurus.png",
+      title: "Ankylosaurus",
+      description:
+        "A herbivorous dinosaur with a body made of solid bones. like a tank. It has a solid club-like tail.",
+    },
+    {
+      src: "/assets/image/NFT/Born/tyrannosaurus.png",
+      title: "tyrannosaurus",
+      description:
+        "The top predator among carnivores, with long, well-developed hind legs and strong muscles.",
+    },
+    {
+      src: "/assets/image/NFT/Born/dilophosaurus.png",
+      title: "dilophosaurus",
+      description:
+        "A carnivorous dinosaur characterized by two bony crests on its head. It has a slender body and a long tail.",
+    },
+    {
+      src: "/assets/image/NFT/Born/cryolophosaurus.png",
+      title: "cryolophosaurus",
+      description:
+        "characterized by fan-shaped crests above the head. It is a carnivorous dinosaur found in Antarctica and called the 'cold lizard'.",
+    },
+    {
+      src: "/assets/image/NFT/Born/corythosaurus.png",
+      title: "corythosaurus",
+      description:
+        "It is called the helmeted lizard. It is a herbivorous dinosaur with a duck-like bill and a large crest on its head.",
+    },
+    {
+      src: "/assets/image/NFT/Born/pachycephalosaurus.png",
+      title: "pachycephalosaurus",
+      description:
+        "Also known as the thick-headed lizard. It is a herbivorous dinosaur with a thick dome head of 20cm.",
+    },
+  ];
   return (
     <Section className="born">
-      <div className="born-inner">Born</div>
+      <div className="born-inner">
+        <Slide data={data} />
+      </div>
     </Section>
   );
 }
